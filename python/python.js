93b731801fc71c68950e55bd2a371af7506a2968
Mujaca <mujaca0@gmail.com>
2023-04-10 22:16:14 +0200

    changes on python spawner

--- python/python.js
+++ python/python.js
@@ -1,13 +1,23 @@
 const { spawn } = require('child_process');
 
 function runPythonScript(file, arguments = []) {
-    return new Promise((resolve, reject => {
+    return new Promise((resolve, reject) => {
         const python = spawn('python3', [`python/${file}`, ...arguments]);
-    
+        const output = [];
+
+        python.stderr.on('data', (data) => {
+            console.log(data.toString());
+            //reject(data.toString());
+        });
+
+        python.stdout.on('data', (data) => {
+            output.push(data.toString());
+        });
+
         python.on('exit', () => {
-            resolve(true);
+            resolve(output);
         })
-    }))
+    })
 }
 
-runPythonScript('python.py');
+exports.runPythonScript = runPythonScript;
